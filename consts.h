855b21f2914d8cf871d441b694f3a79d192fc211
Artemy Bobrovskiy <aabobrovskiy@edu.hse.ru>
2022-04-19 06:41:49 +0300

    Rework testing

---
+++ consts.h
@@ -0,0 +1,8 @@
+//
+// Created by artemy on 17.04.22.
+//
+
+#ifndef FILTERS_CONSTS_H
+#define FILTERS_CONSTS_H
+
+#endif //FILTERS_CONSTS_H
---
+++ testdata.h
@@ -0,0 +1,8 @@
+//
+// Created by artemy on 17.04.22.
+//
+
+#ifndef FILTERS_TESTDATA_H
+#define FILTERS_TESTDATA_H
+
+#endif //FILTERS_TESTDATA_H
